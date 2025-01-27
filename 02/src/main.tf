eee20df79ab9fc336dadcb9f7b3ddce75735cd80
vasionxxx <144676961+vasionxxx@users.noreply.github.com>
2025-01-27 12:51:59 +0300

    Update main.tf

--- 02/src/main.tf
+++ 02/src/main.tf
@@ -1,6 +1,7 @@
 resource "yandex_vpc_network" "develop" {
   name = var.vpc_name
 }
+
 resource "yandex_vpc_subnet" "develop" {
   name           = var.vpc_name
   zone           = var.default_zone
@@ -8,34 +9,70 @@ resource "yandex_vpc_subnet" "develop" {
   v4_cidr_blocks = var.default_cidr
 }
 
+resource "yandex_vpc_subnet" "develop_b" {
+  name           = var.vpc_name_b
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr_b
+}
 
 data "yandex_compute_image" "ubuntu" {
-  family = "ubuntu-2004-lts"
+  family = var.vm_web_image_family
 }
 resource "yandex_compute_instance" "platform" {
-  name        = "netology-develop-platform-web"
-  platform_id = "standart-v4"
+  name        = local.vm_web_name_full
+  platform_id = var.vm_web_platform_id
+
   resources {
-    cores         = 1
-    memory        = 1
-    core_fraction = 5
+    cores         = var.vms_resources["web"].cores
+    memory        = var.vms_resources["web"].memory
+    core_fraction = var.vms_resources["web"].core_fraction
   }
+
   boot_disk {
     initialize_params {
       image_id = data.yandex_compute_image.ubuntu.image_id
     }
   }
+
   scheduling_policy {
-    preemptible = true
+    preemptible = var.vm_web_preemptible
   }
+
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
   }
 
-  metadata = {
-    serial-port-enable = 1
-    ssh-keys           = "ubuntu:${var.vms_ssh_root_key}"
+  metadata = var.metadata
+  
+}
+
+resource "yandex_compute_instance" "db" {
+  name        = local.vm_db_name_full
+  platform_id = var.vm_db_platform_id
+  zone        = yandex_vpc_subnet.develop_b.zone
+  resources {
+    cores         = var.vms_resources["db"].cores
+    memory        = var.vms_resources["db"].memory
+    core_fraction = var.vms_resources["db"].core_fraction
   }
 
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id  # РСЃРїРѕР»СЊР·СѓРµРј С‚РѕС‚ Р¶Рµ РѕР±СЂР°Р· РґР»СЏ РІС‚РѕСЂРѕР№ Р’Рњ
+    }
+  }
+
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop_b.id
+    nat       = true
+  }
+
+  metadata = var.metadata
 }
+
