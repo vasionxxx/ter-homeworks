35c4fbeee0f2388564d537ddd8a1b8021daf2878
vasionxxx <144676961+vasionxxx@users.noreply.github.com>
2025-02-10 22:37:40 +0300

    Update disk_vm.tf

--- 03/src/disk_vm.tf
+++ 03/src/disk_vm.tf
@@ -1,16 +1,16 @@
-# РЎРѕР·РґР°РЅРёРµ 3 РѕРґРёРЅР°РєРѕРІС‹С… РІРёСЂС‚СѓР°Р»СЊРЅС‹С… РґРёСЃРєРѕРІ
 resource "yandex_compute_disk" "storage_disks" {
   count = 3
 
   name     = "storage-disk-${count.index + 1}"
   type     = "network-hdd"
   zone     = var.default_zone
-  size     = 1 # Р Р°Р·РјРµСЂ РґРёСЃРєР° 1 Р“Р±
+  size     = 1
 }
 
-# РЎРѕР·РґР°РЅРёРµ РѕРґРёРЅРѕС‡РЅРѕР№ Р’Рњ "storage"
 resource "yandex_compute_instance" "storage" {
-  name        = "storage"
+  count = 1
+
+  name        = "storage-${count.index + 1}"
   platform_id = "standard-v2"
   zone        = var.default_zone
 
@@ -21,7 +21,7 @@ resource "yandex_compute_instance" "storage" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd81hgrcv6lsnkremf32" # РџСЂРёРјРµСЂ ID РѕР±СЂР°Р·Р° Ubuntu 20.04
+      image_id = data.yandex_compute_image.ubuntu.image_id
       size     = 10
     }
   }
@@ -29,14 +29,13 @@ resource "yandex_compute_instance" "storage" {
   network_interface {
     subnet_id = yandex_vpc_subnet.develop.id
     nat       = true
-    security_group_ids = [yandex_vpc_security_group.example.id] # РќР°Р·РЅР°С‡Р°РµРј РіСЂСѓРїРїСѓ Р±РµР·РѕРїР°СЃРЅРѕСЃС‚Рё
+    security_group_ids = [yandex_vpc_security_group.example.id]
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}" # РСЃРїРѕР»СЊР·СѓРµРј SSH-РєР»СЋС‡
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 
-  # Р”РёРЅР°РјРёС‡РµСЃРєРѕРµ РїРѕРґРєР»СЋС‡РµРЅРёРµ РґРѕРїРѕР»РЅРёС‚РµР»СЊРЅС‹С… РґРёСЃРєРѕРІ
   dynamic "secondary_disk" {
     for_each = yandex_compute_disk.storage_disks
     content {
