cefef479a78ca0612bb9ba959e37e464c079f947
vasionxxx <144676961+vasionxxx@users.noreply.github.com>
2025-02-10 22:37:02 +0300

    Update ansible.tf

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -1,4 +1,3 @@
-# Р›РѕРєР°Р»СЊРЅС‹Рµ РїРµСЂРµРјРµРЅРЅС‹Рµ РґР»СЏ СЃР±РѕСЂР° РґР°РЅРЅС‹С… Рѕ Р’Рњ
 locals {
   webservers = [
     for vm in yandex_compute_instance.web : {
@@ -17,15 +16,14 @@ locals {
   ]
 
   storage = [
-    {
-      name       = yandex_compute_instance.storage.name
-      external_ip = yandex_compute_instance.storage.network_interface[0].nat_ip_address
-      fqdn       = yandex_compute_instance.storage.fqdn
+    for vm in yandex_compute_instance.storage : {
+      name       = vm.name
+      external_ip = vm.network_interface[0].nat_ip_address
+      fqdn       = vm.fqdn
     }
   ]
 }
 
-# РЎРѕР·РґР°РЅРёРµ inventory-С„Р°Р№Р»Р° РґР»СЏ Ansible
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/templates/inventory.tftpl", {
     webservers = local.webservers
